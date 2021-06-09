7ad74978b031ccbb1bf49408efca882fd7df1e03
Abdullahi <abdullahij951@gmail.com>
2021-06-09 08:12:15 +0100

    fix register page bug

--- start/kernel.ts
+++ start/kernel.ts
@@ -22,8 +22,7 @@ import Server from '@ioc:Adonis/Core/Server'
 */
 Server.middleware.register([
   () => import('@ioc:Adonis/Core/BodyParser'),
-  "Adonis/Core/BodyParserMiddleware",
-  "Adonis/Addon/ShieldMiddleware",
+  // () => import("@Adonisjs/Shield"),
 ])
 
 /*
